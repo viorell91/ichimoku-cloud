7f4a57737e63d169b53e34a05dace3d55c9c560b
Viorel Morari <viorell91@yahoo.com>
2021-03-24 17:17:35 +0100

    clean-up. add company name info to df.

--- run_ichimoku.py
+++ run_ichimoku.py
@@ -3,22 +3,20 @@ from utils import *
 import yfinance as yf
 import pandas as pd
 
-ibm = yf.Ticker('IBM')
-period_with_displacement = get_period_with_displacement('6mo')
-df = ibm.history(period=period_with_displacement)
+ticker = yf.Ticker('PFE')
+period_with_displacement = get_period_with_displacement('9mo')
+df = ticker.history(period=period_with_displacement)
 
 df.drop(['Dividends','Stock Splits'], inplace=True, axis=1)
 df.reset_index(inplace=True)
 df.set_index('Date', inplace=True)
 df['Date'] = df.index
+# store ticker's long name to be used in the plot title
+df['Company Name'] = ticker.info['longName']
 
-# Load Sample Data into a dataframe
-#df_old = pd.read_csv('./sample-data/ohcl_sample.csv',index_col=0)
-#df_tsla = pd.read_csv('./sample-data/tsla.csv',index_col=0)
 # Initialize with ohcl dataframe
 i = Ichimoku(df)
 # Generates ichimoku dataframe
 ichimoku_df = i.run()
-ichimoku_df.to_csv('ibm_df_default.csv', index = False) 
 # Plot ichimoku
 i.plot()
